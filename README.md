f8538949d5118bbb9ee95065fb31a833eaab860b
Kate <ezakimatova@gmail.com>
2024-07-26 22:13:13 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flat or Sharp
+
+Simple ear training app
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Theme } from '@radix-ui/themes'
+
+function App() {
+    return (
+        <Theme appearance="dark">
+            <div>tratata</div>
+        </Theme>
+    )
+}
+
+export default App
